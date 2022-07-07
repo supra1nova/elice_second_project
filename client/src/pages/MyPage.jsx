86ed1eba74c9c3c8b3105e716beffa51d36f42b8
minsun <judy.leems@gmail.com>
2022-07-07 12:21:51 +0900

    Feat: global layout, half layout 수정

--- client/src/pages/MyPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Gnb } from '../components/oranisms/Gnb';
-
-export const MyPage = () => {
-  return (
-    <>
-      <h1>you are in MyPage section</h1>
-    </>
-  );
-};
--- client/src/router/GlobalLayout.tsx
+++ client/src/router/GlobalLayout.tsx
@@ -3,16 +3,30 @@ import { Outlet } from 'react-router-dom';
 import { Gnb } from '../components/oranisms/Gnb';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  display: flex;
+const AppContainer = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  background: #fff;
+`;
+
+const GlobalContainer = styled.div``;
+
+const GlobalContainerOutlet = styled.div`
+  position: relative;
+  margin-left: 233px;
 `;
 
 const GlobalLayout = () => {
   return (
-    <Container>
-      <Gnb />
-      <Outlet />
-    </Container>
+    <AppContainer>
+      <GlobalContainer>
+        <Gnb />
+        <GlobalContainerOutlet>
+          <Outlet />
+        </GlobalContainerOutlet>
+      </GlobalContainer>
+    </AppContainer>
   );
 };
 
