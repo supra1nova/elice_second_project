85bee841011929fa3eacb6cdc7c490c29f22f5a2
okdol1 <okdol0611@gmail.com>
2022-07-11 15:32:20 +0900

    Feat: 상세페이지 이미지 슬라이드 구현

---
+++ client/src/components/oranisms/AccountRestaurantsList/AccountRestaurantsList.tsx
@@ -0,0 +1,13 @@
+import SliderImg from '../../molecules/AccountRrestaurantsList/Slider/SliderImg'
+import RestaurantInfo from '../../molecules/AccountRrestaurantsList/RestaurantInfo/RestaurantInfo'
+
+const AccountRestaurantsListTemplate = () => {
+  return (
+    <>
+        <SliderImg />
+        <RestaurantInfo />
+    </>
+  );
+};
+
+export default AccountRestaurantsListTemplate;
