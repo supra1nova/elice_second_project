aaf81a0e92769ce0cc9ab0623b87ae32feda13e4
okdol1 <okdol0611@gmail.com>
2022-07-19 23:24:43 +0900

    Feat: 예약하기 구현 중

--- client/src/components/oranisms/AccountRestaurantsList/RestaurantTitle/template/style.ts
+++ client/src/components/oranisms/AccountRestaurantsList/RestaurantTitle/template/style.ts
@@ -8,24 +8,28 @@ export const FormContainer = styled.form`
     width: 387px;
     border-top: 1px solid ${(props) => props.theme.colors.line};
     padding: 20px 0 0;
+    color: ${(props) => props.theme.colors.font1};
+    ${(props) => props.theme.font.subtitle1}
 `;
 
 export const TimeWrapper = styled.div`
     width: 100%;
-    margin-top: 10px;
+    margin: 10px 0;
     display: flex;
     align-items: center;
     flex-wrap: wrap;
+    gap: 5px;
 `;
 
 export const TimeSelectButton = styled.button`
     min-width: 92px;
     height: 36px;
     border-radius: 5px;
+    ${(props) => props.theme.font.description2}
     ${(props: TimeProps) =>
     props.active
         ? 'background-color : #64AD57; color: white; '
-        : 'background-color: white;border: 1px solid #E5E5E5;'}
+        : 'background-color: white; border: 1px solid #E5E5E5; color: #83867E'}
 `;
 
 export const CountWrapper = styled.div`
@@ -53,3 +57,56 @@ export const Divider = styled.div`
   width: 100%;
   margin: 20px 0;
 `;
+
+export const FormName = styled.div`
+  margin-left: 10px;
+`
+
+export const FormCategory = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const TimeLegend = styled.div`
+  display: flex;
+  ${(props) => props.theme.font.caption}
+  justify-content: end;
+  align-items: center;
+  color: ${(props) => props.theme.colors.font3};
+
+  span {
+    margin-left: 5px;
+  }
+`
+
+export const Box = styled.div`
+  width: 12px;
+  height: 12px;
+  margin-left: 10px;
+`
+
+export const SelectBox = styled(Box)`
+  background-color: ${(props) => props.theme.colors.main1};
+`
+export const NoneBox = styled(Box)`
+  background-color: ${(props) => props.theme.colors.line};
+`
+
+export const BookerInfo = styled.div`
+  padding: 10px 0 0;
+  ${(props) => props.theme.font.caption}
+
+  div {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+  }
+
+  div:last-child {
+    align-items: flex-start;
+  }
+
+  div > span {
+    width: 55px;
+  }
+`
