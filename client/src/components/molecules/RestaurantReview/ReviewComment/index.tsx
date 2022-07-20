5c6ac75a8a50d8d540adc5cf62eabcd4230729b2
okdol1 <okdol0611@gmail.com>
2022-07-20 11:49:18 +0900

    Feat: 회원 role에 따른 뷰 수정 중

--- client/src/components/molecules/RestaurantReview/ReviewComment/index.tsx
+++ client/src/components/molecules/RestaurantReview/ReviewComment/index.tsx
@@ -18,8 +18,23 @@ const ReviewComment = () => {
       ownerComment: null,
     },
   ]);
+  const [role, setRole] = useState<string | null | undefined>(null)
+  const isNotUser = role === undefined
+  const isUser = role === 'user'
+  const isOwner = role === 'owner'
+  const isAdmin = role === 'admin'
 
   useEffect(() => {
+    API.userGet('/api/users/user').then((res) => {
+      console.log(res)
+      if(res === undefined) {
+        setRole(undefined)
+      } else {
+        setRole(res.role)
+      }
+      // 여기 email이랑 리뷰 email이링 값이 같으면 삭제버튼을 보여주고
+  });
+
     const REGNumber = window.location.href.split('/')[5];
 
     API.get(`/api/reviews/${REGNumber}`).then((res) => {
@@ -35,15 +50,19 @@ const ReviewComment = () => {
           // role을 체크해서 유저일경우 -> UserReviewDetail
           // role을 체크해서 사장님일경우 -> OwnerReviewDetail
           // role을 체크해서 관리자일경우 -> AdminReviewDetail
-          <ReviewDetail
-            key={index}
-            email={item.email}
-            createdAt={item.createdAt}
-            comment={item.comment}
-            rating={item.rating}
-            ownerComment={item.ownerComment}
-            reserveId={item.reserveId}
-          />
+          {
+            isNotUser
+            // ? <ReviewDetail
+            //     key={index}
+            //     email={item.email}
+            //     createdAt={item.createdAt}
+            //     comment={item.comment}
+            //     rating={item.rating}
+            //     ownerComment={item.ownerComment}
+            //     reserveId={item.reserveId}
+            //   />
+          }
+          
         );
       })}
     </UI.StyledContent>
