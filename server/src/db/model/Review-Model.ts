edeb7b4bb5820bac451240faa1c5e8bb54deae9c
supra1nova <supra1nova@gmail.com>
2022-07-21 23:48:29 +0900

    [feature] review 관련 코드 수정

--- server/src/db/model/Review-Model.ts
+++ server/src/db/model/Review-Model.ts
@@ -7,15 +7,7 @@ import { Rating } from '../entity/Rating';
  * Loads all posts from the database.
  */
 export class ReviewModel {
-  async findReviewByReserveId(reserveId: number) {
-    const reviewRepository = AppDataSource.getRepository(Review);
-    // get a post repository to perform operations with post
-    const review = await reviewRepository.findOneBy({
-      reserveId: reserveId,
-    });
-    return review;
-  }
-
+  // 1-1. 유저 리뷰 생성
   async create(reviewInfo: reviewInfo) {
     await AppDataSource.createQueryBuilder()
       .insert()
@@ -24,6 +16,7 @@ export class ReviewModel {
       .execute();
   }
 
+  // 1-2. 업주 리뷰 생성
   async reply(reserveId: number, ownerComment: string) {
     await AppDataSource.createQueryBuilder()
       .update(Review)
@@ -32,30 +25,21 @@ export class ReviewModel {
       .execute();
   }
 
-  async replyDelete(reserveId: number) {
-    await AppDataSource.query(` UPDATE Review SET ownerComment=NULL WHERE reserveId=${reserveId}`);
-      // .update(Review)
-      // .set({ ownerComment: a })
-      // .where('reserveId = :reserveId', { reserveId: reserveId })
-      // .execute();
-  }
-
-  //
-  async countAll() {
+  // 2-1. 특정 범위(페이지) 위치한 리뷰 조회
+  async getInRange(page: number, perPage: number) {
     const reviewRepository = AppDataSource.getRepository(Review);
-    const count = await reviewRepository.count({});
-    return count;
-  }
 
-  async countAllByREGNumber(REGNumber: string) {
-    const reviewRepository = AppDataSource.getRepository(Review);
-    const reviewsNumber = await reviewRepository.count({
-      where: { REGNumber: REGNumber },
+    const reviewsInRange = await reviewRepository.find({
+      order: {
+        createdAt: 'DESC',
+      },
+      skip: perPage * (page - 1),
+      take: perPage,
     });
-    return reviewsNumber;
+    return reviewsInRange;
   }
 
-  // 4-1. 특정 범위(페이지) 위치한 제품 정보 조회
+  // 2-2. 특정 범위(페이지) 위치한 리뷰 조회 - REGNumber 이용
   async getInRangeByREGNumber(
     REGNumber: string,
     page: number,
@@ -74,20 +58,17 @@ export class ReviewModel {
     return reviewsInRange;
   }
 
-  // 4-2. 특정 범위(페이지) 위치한 제품 정보 조회
-  async getInRange(page: number, perPage: number) {
+  // 2-3. 특정 리뷰 조회
+  async findReviewByReserveId(reserveId: number) {
     const reviewRepository = AppDataSource.getRepository(Review);
-
-    const reviewsInRange = await reviewRepository.find({
-      order: {
-        createdAt: 'DESC',
-      },
-      skip: perPage * (page - 1),
-      take: perPage,
+    // get a post repository to perform operations with post
+    const review = await reviewRepository.findOneBy({
+      reserveId: reserveId,
     });
-    return reviewsInRange;
+    return review;
   }
 
+  // 3-1. 사용자 리뷰 삭제
   async deleteReview(reserveId: number) {
     await AppDataSource.createQueryBuilder()
       .delete()
@@ -96,6 +77,32 @@ export class ReviewModel {
       .execute();
   }
 
+  // 3-2. 업주 리뷰 삭제
+  async replyDelete(reserveId: number) {
+    await AppDataSource.query(
+      ` UPDATE Review SET ownerComment=NULL WHERE reserveId=${reserveId}`,
+    );
+    // .update(Review)
+    // .set({ ownerComment: a })
+    // .where('reserveId = :reserveId', { reserveId: reserveId })
+    // .execute();
+  }
+
+  // 4-1. 전체 리뷰 숫자 카운트
+  async countAll() {
+    const reviewRepository = AppDataSource.getRepository(Review);
+    const count = await reviewRepository.count({});
+    return count;
+  }
+
+  // 4-2. 특정 업체 리뷰 숫자 카운트
+  async countAllByREGNumber(REGNumber: string) {
+    const reviewRepository = AppDataSource.getRepository(Review);
+    const reviewsNumber = await reviewRepository.count({
+      where: { REGNumber: REGNumber },
+    });
+    return reviewsNumber;
+  }
 }
 
 const reviewModel = new ReviewModel();
