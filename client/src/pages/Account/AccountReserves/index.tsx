94071d1805e4d84cac9585a2f278cd879b2e7b88
grey <kdc4925123@gmail.com>
2022-07-22 11:39:14 +0900

    Fix : accountReserve 수정

--- client/src/pages/Account/AccountReserves/index.tsx
+++ client/src/pages/Account/AccountReserves/index.tsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as API from '../../../api/api';
 import AccountReservesListOwner from './template/AccountReservesListOwner';
 import AccountReservesListUser from './template/AccountReservesListUser';
 const AccountReserves = () => {
+  const navigate = useNavigate();
   const [role, setRole] = useState<string>();
   useEffect(() => {
     API.userGet('/api/users/user').then((res) => {
+      console.log(res);
       setRole(res.role);
     });
   }, []);
 
-  const Navigate = () => {
-    window.location.href = '/';
-    return <></>;
-  };
+  // const Navigate = () => {
+  //   window.location.href = '/';
+  //   return <></>;
+  // };
+  console.log(role);
+
+  if (role === 'admin') {
+    navigate('/');
+  }
 
   if (role === 'owner') {
     return <AccountReservesListOwner />;
-  } else if (role === 'admin') {
-    return <Navigate />;
   } else {
     return <AccountReservesListUser />;
   }
