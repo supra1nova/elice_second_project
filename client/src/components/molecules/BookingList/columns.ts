99ab25f0d4c0c626e7205c2aed5531f21797ead6
okdol1 <okdol0611@gmail.com>
2022-07-22 16:54:44 +0900

    Feat: Owner 예약조회 테이블 구현

---
+++ client/src/components/molecules/BookingList/columns.ts
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+    {
+        Header : '예약번호',
+        accessor : 'reserveId'
+    },
+    {
+        Header : '예약자명',
+        accessor : 'name'
+    },
+    {
+        Header : '이메일',
+        accessor : 'email'
+    },
+    {
+        Header : '인원',
+        accessor : 'number'
+    },
+    {
+        Header : '전화번호',
+        accessor : 'phoneNumber'
+    },
+    {
+        Header : '요청사항',
+        accessor : 'comment'
+    },
+    // {
+    //     Header : '예약날짜',
+    //     accessor : 'date'
+    // },
+];
