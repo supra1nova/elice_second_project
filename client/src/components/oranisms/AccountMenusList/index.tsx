dd939799150632142683e2f41ffccaf1d9e7e411
grey <kdc4925123@gmail.com>
2022-07-22 13:15:04 +0900

    Feat : table 구성1

--- client/src/components/oranisms/AccountMenusList/index.tsx
+++ client/src/components/oranisms/AccountMenusList/index.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import AccountHeader from '../../molecules/AccountHeader';
 import { SECTION } from '../../../constants/title';
 import * as UI from './style';
+import Table from './MenuTable';
+import * as API from '../../../api/api';
 
 const AccountMenusList = () => {
-  return (
-    <UI.Container>
-      <AccountHeader title={SECTION.MENU_MANAGEMENT} />
-      테이블이들어갑니다
-    </UI.Container>
+  const [menus, setMenus] = useState([]);
+  const getData = async () => {
+    const result = await API.get(`/api/menus/12345678`);
+    setMenus(result);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(menus);
+  const columns = useMemo(
+    () => [
+      {
+        accessor: 'menuId',
+        Header: '#',
+      },
+      {
+        accessor: 'name',
+        Header: '메뉴명',
+      },
+      {
+        accessor: 'price',
+        Header: '가격',
+      },
+      // {
+      //   accessor: 'phone',
+      //   Header: '관리',
+      // },
+    ],
+    [],
   );
+
+  const data = useMemo(
+    () =>
+      menus.map((e: any) => ({
+        menuId: e.menuId,
+        name: e.name,
+        price: e.price,
+      })),
+    [],
+  );
+  if (menus.length > 0) {
+    return (
+      <UI.Container>
+        <AccountHeader title={SECTION.MENU_MANAGEMENT} />
+        <Table columns={columns} data={data} />
+      </UI.Container>
+    );
+  } else {
+    return (
+      <UI.Container>
+        <AccountHeader title={SECTION.MENU_MANAGEMENT} />
+      </UI.Container>
+    );
+  }
 };
 
 export default AccountMenusList;
