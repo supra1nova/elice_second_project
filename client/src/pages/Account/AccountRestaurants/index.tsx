2fa01a557b153eb1f635a45daedf03c874e0467a
minsun <judy.leems@gmail.com>
2022-07-22 12:02:46 +0900

    Feat: Role별 Router 수정

--- client/src/pages/Account/AccountRestaurants/index.tsx
+++ client/src/pages/Account/AccountRestaurants/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as API from '../../../api/api';
 import AccountRestaurantsListUser from './template/AccountRestaurantsListUser';
 import AccountRestaurantsListAdmin from './template/AccountRestaurantsListAdmin';
@@ -9,10 +10,14 @@ const AccountRestaurants = () => {
   const [role, setRole] = useState<string>();
   const REGNumber = localStorage.getItem('REGNumber');
 
-  useEffect(() => {
+  const getData = async () => {
     API.userGet('/api/users/user').then((res) => {
       setRole(res.role);
     });
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
 
   if (role === 'admin') {
